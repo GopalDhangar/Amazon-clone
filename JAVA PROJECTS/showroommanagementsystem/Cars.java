5c9fa4cabc1aae766c83517f720fdc385946ca63
Gopal_Dhangar <144979013+GopalDhangar@users.noreply.github.com>
2024-08-23 00:13:55 +0530

    Update Cars.java

--- JAVA PROJECTS/showroommanagementsystem/Cars.java
+++ JAVA PROJECTS/showroommanagementsystem/Cars.java
@@ -8,39 +8,36 @@ public class Cars extends Showroom implements utility {
     String car_fuel_type;
     int car_price;
     String car_type;
-    String car_transMission;total_cars_in_stock=0;
+    String car_transmission;
 
     @Override
     public void get_details() {
-        System.out.println("Car Name: " + car_name);
-        System.out.println("Car Color: " + car_color);
-        System.out.println("Car Fuel Type: " + car_fuel_type);
-        System.out.println("Car Price: " + car_price);
-        System.out.println("Car Type: " + car_type);
-        System.out.println("Car Transmission: " + car_transMission);
-        System.out.println("Total Cars In Stock: " + total_cars_in_stock);
-
+        System.out.println("NAME: " + car_name);
+        System.out.println("COLOR: " + car_color);
+        System.out.println("FUEL TYPE: " + car_fuel_type);
+        System.out.println("PRICE: " + car_price);
+        System.out.println("CAR TYPE: " + car_type);
+        System.out.println("TRANSMISSION: " + car_transmission);
     }
 
     @Override
     public void set_details() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("======================= *** ENTER SHOWROOM DETAILS *** ====================");
+        System.out.println("======================= *** ENTER CAR DETAILS *** =======================");
         System.out.println();
-        System.out.println("Car Name: ");
+        System.out.print("CAR NAME: ");
         car_name = sc.nextLine();
-        System.out.println("car Color: ");
+        System.out.print(("CAR COLOR: "));
         car_color = sc.nextLine();
-        System.out.println("Car Fuel Type: ");
+        System.out.print("CAR FUEL TYPE(PETROL/DIESEL): ");
         car_fuel_type = sc.nextLine();
-        System.out.println("Car Price: ");
+        System.out.print("CAR PRICE: ");
         car_price = sc.nextInt();
-        System.out.println("Car Type: ");
+        sc.nextLine();
+        System.out.print("CAR TYPE(SEDAN/SUV/HATCHBACK): ");
         car_type = sc.nextLine();
-        System.out.println("Car Transmission: ");
-        car_transMission=sc.nextLine();
+        System.out.print("TRANSMISSION TYPE(AUTOMATIC/MANUAL): ");
+        car_transmission = sc.nextLine();
         total_cars_in_stock++;
-
-
-    
+    }
 }
